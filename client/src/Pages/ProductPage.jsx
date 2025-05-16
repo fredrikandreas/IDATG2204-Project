269f2e332a3caaf3ae4e95504634d02180d53f99
fredrawi <fredrikandreas03@gmail.com>
2025-05-16 14:30:29 +0200

    Finished home/products page

--- client/src/Pages/ProductPage.jsx
+++ client/src/Pages/ProductPage.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import ProductItem from "../Components/ProductItem/ProductItem";
+import Button from "../Components/Button/Button";
+import {getProduct} from "../utils/getProduct";
+import {getAllProducts} from "../utils/getAllProducts";
 
 const ProductPage = () => {
-    const { productId } = useParams();
+    const id = window.location.pathname.split("/")[2];
+
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                const prod = await getProduct(id);
+                console.log(prod);
+                setProduct(prod);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchProducts().then();
+    }, []);
+
+    console.log(product);
 
     return (
         <div>
-            <h1>Product page</h1>
-            <p>{productId}</p>
+            <h1>Product Page</h1>
+            <ProductItem
+                image={`${process.env.REACT_APP_BACKEND}${product['image_url']}`}
+                name={product['name']}
+                price={product['price']}
+                quantity={product['stock_quantity']}
+            />
+            <Button type="dark" text="Add to cart" path={"/cart"}></Button>
         </div>
     )
 }
--- client/src/utils/getProduct.js
+++ client/src/utils/getProduct.js
@@ -1,3 +1,8 @@
-export const getProduct = (id) => {
-
-};
+export async function getProduct(id) {
+    const backendUrl = `${process.env.REACT_APP_BACKEND}/api/products/${id}`;
+    const res = await fetch(backendUrl);
+    if (!res.ok) {
+        throw new Error(`Failed to load product: ${res.status} ${res.statusText}`);
+    }
+    return await res.json();
+}

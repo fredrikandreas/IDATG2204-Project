044107a98e2d00dbb2da49691d239e8f3eb5d788
fredrawi <fredrikandreas03@gmail.com>
2025-05-20 12:28:16 +0200

    Reverted back to original getAllProducts

--- client/src/utils/getAllProducts.js
+++ client/src/utils/getAllProducts.js
@@ -1,6 +1,5 @@
 export async function getAllProducts() {
     const backendUrl = `${process.env.REACT_APP_BACKEND}/api/products`;
-    console.log(backendUrl);
     const res = await fetch(backendUrl);
     if (!res.ok) {
         throw new Error(`Failed to load products: ${res.status} ${res.statusText}`);

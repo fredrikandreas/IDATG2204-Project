990a9aeab82ae7ff8d4e986145b529996d4998ac
fredrawi <fredrikandreas03@gmail.com>
2025-05-20 15:19:50 +0200

    Small adjustments in the styling for ProductItem

--- client/src/Components/ProductItem/ProductItem.jsx
+++ client/src/Components/ProductItem/ProductItem.jsx
@@ -3,7 +3,7 @@ import './ProductItem.css';
 
 const ProductItem = React.memo(({ dir, image, name, price, description, quantity }) => {
     return (
-        <div className="product-container" style={{flexDirection: dir}}>
+        <div className="product-container" style={{flexDirection: dir, gap: dir === 'row' ? '48px' : '16px'}}>
             <div className="product-image" style={{backgroundImage: `url(${image})`}}></div>
             <div className="product-info" style={{width: dir === 'row' ? '50%' : '100%'}}>
                 <h1>{dir === 'row' ? name : price + ",-"}</h1>

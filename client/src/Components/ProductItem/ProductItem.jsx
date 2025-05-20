099dbd0854ebbd6fa8aca81bf0c5b93a25ac0099
fredrawi <fredrikandreas03@gmail.com>
2025-05-20 15:13:30 +0200

    Updated ProductItem to display Price as title when on products and name as title when in productPage

--- client/src/Components/ProductItem/ProductItem.jsx
+++ client/src/Components/ProductItem/ProductItem.jsx
@@ -6,8 +6,8 @@ const ProductItem = React.memo(({ dir, image, name, price, description, quantity
         <div className="product-container" style={{flexDirection: dir}}>
             <div className="product-image" style={{backgroundImage: `url(${image})`}}></div>
             <div className="product-info" style={{width: dir === 'row' ? '50%' : '100%'}}>
-                <h1>{price},-</h1>
-                <h2>{name}</h2>
+                <h1>{dir === 'row' ? name : price + ",-"}</h1>
+                <h2>{dir === 'row' ? price + ",-" : name}</h2>
                 <h3>{description}</h3>
                 <h3 className="kvant">{quantity} på lager</h3>
             </div>

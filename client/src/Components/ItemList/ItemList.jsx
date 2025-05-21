9b3471858cfd98e41b1f5cdc88033a89d9c31bdf
fredrawi <fredrikandreas03@gmail.com>
2025-05-21 12:24:41 +0200

    Removed unused code from ItemList.jsx

--- client/src/Components/ItemList/ItemList.jsx
+++ client/src/Components/ItemList/ItemList.jsx
@@ -2,27 +2,12 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './ItemList.css';
 import ProductItem from '../ProductItem/ProductItem';
+import {BACKEND_URL} from "../../utils/constants";
 
 export default function ItemList({ productList }) {
-    const containerRef = useRef(null);
-    const itemWidth = 200;
-    const [columns, setColumns] = useState(1);
-
-    useEffect(() => {
-        const updateColumns = () => {
-            if (!containerRef.current) return;
-            const width = containerRef.current.offsetWidth;
-            const cols = Math.max(1, Math.floor(width / itemWidth));
-            setColumns(cols);
-        };
-        updateColumns();
-        window.addEventListener('resize', updateColumns);
-        return () => window.removeEventListener('resize', updateColumns);
-    }, []);
-
     return (
         <div className="cont">
-            <div className="item-list-container" ref={containerRef}>
+            <div className="item-list-container">
                 {productList.length === 0 ? (
                     <p className="no-results">Loading...</p>
                 ) : (
@@ -34,7 +19,7 @@ export default function ItemList({ productList }) {
                         >
                             <ProductItem
                                 dir="column"
-                                image={`${process.env.REACT_APP_BACKEND}${item['image_url']}`}
+                                image={BACKEND_URL + item['image_url']}
                                 name={item['name']}
                                 price={item['price']}
                                 description=""

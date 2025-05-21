621ee83ba94c0ea95d918f2651328f0e6bd1191b
fredrawi <fredrikandreas03@gmail.com>
2025-05-21 18:50:18 +0200

    Updated registerUser.js to include all parameters for registration

--- client/src/utils/registerUser.js
+++ client/src/utils/registerUser.js
@@ -1,12 +1,32 @@
 import { BACKEND_URL, PATH_REGISTER } from "./constants";
 
-export const registerUser = async (username, password, email) => {
+export const registerUser = async (username,
+                                   password,
+                                   email, firstName,
+                                   lastName,
+                                   street,
+                                   city,
+                                   postcode,
+                                   phoneNumber,
+                                   dateOfBirth,
+                                   ) => {
     let api_url = BACKEND_URL + PATH_REGISTER;
 
     const res = await fetch(api_url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password, email }),
+        body: JSON.stringify({
+            username,
+            password,
+            email,
+            firstName,
+            lastName,
+            street,
+            city,
+            postcode,
+            phoneNumber,
+            dateOfBirth,
+        }),
     });
     const data = await res.json();
     return { ok: res.ok, data };

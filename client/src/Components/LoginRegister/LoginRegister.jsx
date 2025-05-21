596af7c5d2cb11cb0c1d33a1ffc9a4af98804cf6
fredrawi <fredrikandreas03@gmail.com>
2025-05-21 18:46:55 +0200

    Added all parameters for registration

--- client/src/Components/LoginRegister/LoginRegister.jsx
+++ client/src/Components/LoginRegister/LoginRegister.jsx
@@ -10,6 +10,14 @@ const LoginRegister = ({ mode }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [street, setStreet] = useState('');
+    const [city, setCity] = useState('');
+    const [postcode, setPostcode] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -23,7 +31,18 @@ const LoginRegister = ({ mode }) => {
     const handleLogin = async (e) => {
         e.preventDefault();
         setLoading(true);
-        const { ok, data } = await loginUser(username, password);
+        const { ok, data } = await loginUser(
+            username,
+            password,
+            email,
+            firstName,
+            lastName,
+            street,
+            city,
+            postcode,
+            phoneNumber,
+            dateOfBirth,
+        );
         setLoading(false);
         if (ok && data.token) {
             localStorage.setItem('token', data.token);
@@ -40,6 +59,7 @@ const LoginRegister = ({ mode }) => {
 
     const handleRegister = async (e) => {
         e.preventDefault();
+        console.log(dateOfBirth);
         setLoading(true);
         const { ok, data } = await registerUser(username, password, email);
         setLoading(false);
@@ -79,6 +99,75 @@ const LoginRegister = ({ mode }) => {
                     onChange={(e) => setEmail(e.target.value)}
                     required
                 />
+                <div className="reg-side" style={{display: mode === 'register' ? 'flex' : 'none'}}>
+                    <input
+                        style={{display: mode === 'register' ? 'block' : 'none'}}
+                        disabled={mode === 'login'}
+                        placeholder="First name"
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        required
+                    />
+                    <input
+                        style={{display: mode === 'register' ? 'block' : 'none'}}
+                        disabled={mode === 'login'}
+                        placeholder="Last name"
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        required
+                    />
+                </div>
+                <input
+                    style={{display: mode === 'register' ? 'block' : 'none'}}
+                    disabled={mode === 'login'}
+                    placeholder="Street"
+                    type="text"
+                    value={street}
+                    onChange={(e) => setStreet(e.target.value)}
+                    required
+                />
+                <div className="reg-side" style={{display: mode === 'register' ? 'flex' : 'none'}}>
+                    <input
+                        style={{display: mode === 'register' ? 'block' : 'none'}}
+                        disabled={mode === 'login'}
+                        placeholder="City"
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        required
+                    />
+                    <input
+                        style={{display: mode === 'register' ? 'block' : 'none'}}
+                        disabled={mode === 'login'}
+                        placeholder="Postcode"
+                        type="number"
+                        value={postcode}
+                        onChange={(e) => setPostcode(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="reg-side" style={{display: mode === 'register' ? 'flex' : 'none'}}>
+                    <input
+                        style={{display: mode === 'register' ? 'block' : 'none'}}
+                        disabled={mode === 'login'}
+                        placeholder="Phone number"
+                        type="tel"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                        required
+                    />
+                    <input
+                        style={{display: mode === 'register' ? 'block' : 'none'}}
+                        disabled={mode === 'login'}
+                        placeholder="Date of birth"
+                        type="date"
+                        value={dateOfBirth}
+                        onChange={(e) => setDateOfBirth(e.target.value)}
+                        required
+                    />
+                </div>
                 <button type="submit" disabled={loading}>
                     <h3>
                         {loading ? (mode === 'register' ? 'Registering...' : 'Logging in...') : (mode === 'register' ? 'Register' : 'Login')}
